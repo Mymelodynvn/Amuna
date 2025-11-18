360463d297b2720d1c2f75bf5856cda0140bd0e3
Mymelodynvn <keiberlys_guedez@soy.sena.edu.co>
2025-11-18 11:13:57 -0500

    carga de config de la db y el example de variables de entorno

---
+++ back/src/config/typeorm.ts
@@ -0,0 +1,23 @@
+import { registerAs } from '@nestjs/config';
+import { config as dotenvConfig } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenvConfig({path: '.env.development'});
+
+const config: DataSourceOptions ={
+    type: 'postgres',
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['dist/migrations/*{.ts, .js}'],
+    logging: false, //muestra en la consola la interacción con la db
+    synchronize: true, // sincroniza las entidades con la db
+    dropSchema: false, //limpia, borra y crea nuevamente las entidades. Siempre en false porque puede borrar la DB
+};
+
+export default registerAs('typeorm',() => config);
+export const connectionSource = new DataSource(config);
